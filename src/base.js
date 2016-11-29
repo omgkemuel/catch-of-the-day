62a0b4928072e1f9f3e39260d58b96a1b023db0f
Kemuel McDaniel <kemuel.mcdaniel.asurion.com>
2016-11-29 11:51:18 -0600

    added Firebase connection

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyCC9JvKW2CZ_nxF6LknOi_MF1cPDmrP-pQ",
+    authDomain: "catch-of-the-day-kemuel.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-kemuel.firebaseio.com",
+});
+
+export default base;
